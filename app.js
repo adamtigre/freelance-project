73f9f36b6e35c6f61d4102b522bf0f055fb84713
unknown <peteradamu19@yahoo.com>
2022-05-16 09:54:19 +0100

    .

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 let popped = 0;
 
-document.addEventListener('mouseover', function(e){
-    
-    if (e.target.className === "balloon"){
-        
-                e.target.style.backgroundColor = "#ededed";
-                e.target.textContent = "POP!";
-                popped++;
-                removeEvent(e);
-                checkAllPopped();
-    }   
+document.addEventListener("mouseover", function (e) {
+  if (e.target.className === "balloon") {
+    e.target.style.backgroundColor = "#ededed";
+    e.target.textContent = "POP!";
+    popped++;
+    removeEvent(e);
+    checkAllPopped();
+  }
 });
 
-function checkAllPopped(){
-    if (popped === 24){
-        console.log('all popped!');
-        let gallery = document.querySelector('#balloon-gallery');
-        let message = document.querySelector('#yay-no-balloons');
-        gallery.innerHTML = '';
-        message.style.display = 'block';
-    }
-};
+function checkAllPopped() {
+  if (popped === 24) {
+    console.log("all popped!");
+    let gallery = document.querySelector("#balloon-gallery");
+    let message = document.querySelector("#yay-no-balloons");
+    gallery.innerHTML = "";
+    message.style.display = "block";
+  }
+}
 
-function removeEvent(e){
-    e.target.removeEventListener('mouseover', function(){
-        
-    })
-};
-
+function removeEvent(e) {
+  e.target.removeEventListener("mouseover", function () {});
+}
