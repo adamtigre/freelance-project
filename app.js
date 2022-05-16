0621fd2191a5c492969e06c18b9ac7fc7aee2060
unknown <peteradamu19@yahoo.com>
2022-05-16 08:55:42 +0100

    frontend javascript

---
+++ app.js
@@ -0,0 +1,30 @@
+let popped = 0;
+
+document.addEventListener('mouseover', function(e){
+    
+    if (e.target.className === "balloon"){
+        
+                e.target.style.backgroundColor = "#ededed";
+                e.target.textContent = "POP!";
+                popped++;
+                removeEvent(e);
+                checkAllPopped();
+    }   
+});
+
+function checkAllPopped(){
+    if (popped === 24){
+        console.log('all popped!');
+        let gallery = document.querySelector('#balloon-gallery');
+        let message = document.querySelector('#yay-no-balloons');
+        gallery.innerHTML = '';
+        message.style.display = 'block';
+    }
+};
+
+function removeEvent(e){
+    e.target.removeEventListener('mouseover', function(){
+        
+    })
+};
+
